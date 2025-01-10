153a8233cb85bd378e142c065dd83c91eadfbb7a
薛兴越 <x13610607718@163.com>
2025-01-10 16:27:36 +0800

    Initial commit

---
+++ src/test/java/cn/haxlab/server/MySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.haxlab.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
